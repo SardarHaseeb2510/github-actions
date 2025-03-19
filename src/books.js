d55380b27b245d582ee59eeb8187d3fd00b04242
shahmeer631 <shahmeerkhokhar365@gmail.com>
2025-03-19 10:03:19 +0500

    second commit

--- src/books.js
+++ src/books.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const filePath = "./Data/books.json";
 
-// Load existing data
+
 const loadBooks = () => {
     if (fs.existsSync(filePath)) {
         const data = fs.readFileSync(filePath);
@@ -11,12 +11,12 @@ const loadBooks = () => {
     return [];
 };
 
-// Save book data
+
 const saveBooks = (books) => {
     fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
 };
 
-// Add a new book
+
 const lendBook = (title, author, borrower, dueDate, category) => {
     const books = loadBooks();
     books.push({ title, author, borrower, dueDate, category });
@@ -24,7 +24,7 @@ const lendBook = (title, author, borrower, dueDate, category) => {
     return "Book added successfully!";
 };
 
-// View books
+
 const viewBooks = (filter = {}) => {
     let books = loadBooks();
 
