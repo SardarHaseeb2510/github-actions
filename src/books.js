4874a06ff429f14aeadf587b096f22ea73c5d95b
SardarHaseeb2510 <147981914+SardarHaseeb2510@users.noreply.github.com>
2025-03-19 14:21:42 +0500

    commit

--- src/books.js
+++ src/books.js
@@ -4,37 +4,54 @@ const filePath = "./Data/books.json";
 
 
 const loadBooks = () => {
-    if (fs.existsSync(filePath)) {
+    if (fs.existsSync(filePath))
+         {
         const data = fs.readFileSync(filePath);
+
         return JSON.parse(data);
     }
+
     return [];
 };
 
 
 const saveBooks = (books) => {
+
     fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
 };
 
 
 const lendBook = (title, author, borrower, dueDate, category) => {
+   
     const books = loadBooks();
+
     books.push({ title, author, borrower, dueDate, category });
+
     saveBooks(books);
+
     return "Book added successfully!";
 };
 
 
 const viewBooks = (filter = {}) => {
+    
     let books = loadBooks();
 
-    if (filter.category) {
+
+    if (filter.category)
+     {
         books = books.filter(book => book.category === filter.category);
     }
-    if (filter.borrower) {
+
+
+    if (filter.borrower) 
+      {
         books = books.filter(book => book.borrower === filter.borrower);
     }
+
     return books;
 };
+
+
 
 module.exports = { lendBook, viewBooks };
--- testScript.js
+++ testScript.js
@@ -1,15 +1,18 @@
 const { lendBook, viewBooks } = require("./src/books");
 
 const testLendingSystem = () => {
-    console.log("Running tests");
+    
+   
 
     lendBook("book1", "abc", "xyz", "2026-07-04", "Fiction");
+
     lendBook("1999", "abc", "xyz", "2026-05-9", "NonFiction");
 
     const fictionBooks = viewBooks({ category: "Fiction" });
+
     console.log("Fiction Books:", fictionBooks);
 
-    console.log("Tests completed successfully!");
+   
 };
 
 testLendingSystem();
